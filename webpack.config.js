6455b3375a182982aa38b1a8e9acf7ac7236504b
PoeticBliss <meremezeukah@gmail.com>
2016-09-04 02:07:40 -0400

    added eslint preloader

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,22 @@ const webpack = require('webpack');
 var PROD = JSON.parse(process.env.PROD_ENV || '0');
 
 module.exports = {
-  entry: __dirname + "/src/index.js",
+  entry: __dirname + "/src/index.es6",
   devtool: "source-map",
   output: {
     path: __dirname + "/build/",
     filename: PROD ? "elitejax.min.js" : "elitejax.js"
   },
   module: {
+    preLoaders: [
+      { test: /\.es6?$/, exclude: /node_modules/, loader: 'eslint-loader', include: __dirname + '/' }
+    ],
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel" }
+      { test: /\.es6$/, exclude: /node_modules/, loader: "babel", query: { presets: ['es2015'] } }
     ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.es6']
   },
   plugins: PROD ? [
     new webpack.optimize.UglifyJsPlugin({
