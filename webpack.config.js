d0a3c644f335dc81ada069f0d3f269cb889867d1
ghostffcode <meremezeukah@gmail.com>
2016-10-16 11:51:09 -0400

    modified dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
 const webpack = require('webpack');
-var PROD = JSON.parse(process.env.PROD_ENV || '0');
+const PROD = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: __dirname + "/src/index.es6",
   devtool: "source-map",
   output: {
     path: __dirname + "/build/",
-    filename: PROD ? "elitejax.min.js" : "elitejax.js",
-    library: 'elitejax',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
+    filename: PROD ? "elitejax.min.js" : "elitejax.js"
   },
   module: {
     preLoaders: [
